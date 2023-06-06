5b47adae0d5d11143def16396e652e7ef8690e59
JOAO VITOR NUNES CARDOSO <1800544@fmu.interno>
2023-06-06 20:28:57 -0300

    criação de inserts para o banco

--- doidos_e_dados_em_mysql.sql
+++ doidos_e_dados_em_mysql.sql
@@ -79,3 +79,14 @@ per_sobrevivencia INT NOT NULL,
 CONSTRAINT pk_personagem PRIMARY KEY (cd_personagem),
 CONSTRAINT fk_usuarios_personagem FOREIGN KEY (cd_usuario) REFERENCES tb_usuarios(cd_usuario) 
 )
+
+INSERT INTO  `tb_ficha_personagem`(`cd_usuario`,`nome_jogador`,`nome_personagem`, `raca_personagem`, `antecedente_personagem`, `tendencia_personagem`, `nome_personagem``atr_forca`, `atr_destreza`, 
+`atr_constituicao`, `atr_inteligencia`,`atr_sabedoria`, `atr_carisma`, 
+    `tdr_forca`, `tdr_destreza`, `tdr_constituicao`, `tdr_inteligencia`,`tdr_sabedoria`, `tdr_carisma`, 
+    `percp_passiva`, `bonus_profici`, `deslocamento`, `iniciativa` , `classe_armadura` , `inspiracao`, 
+    `vida_maxima`, `vida_atual`, `vida_temporaria`, `armor`, `armas`, `ferramentos`, `linguagens`, `historia`,
+    `magias`, `extras`, `per_acrobacia`, `per_arcanismo`, `per_atletismo`, `per_atuacao`, `per_blefar`, `per_furtividade`,
+    `per_historia`, `per_intimidacao`, `per_investigacao`, `per_lidar_animais`, `per_medicina`, `per_natureza`, 
+    `per_persuasao`, `per_prestidigitacao`, `per_religiao`, `per_sobrevivencia`)VALUES(1,"Joao Vitor", "Beatrice", "Elfa", "Forasteiro", 
+    "Caotic Evil", 10, 15, 13, 14, 20, 20, 0, 2, 1, 2 ,5 ,5 ,0 ,2 ,1 ,2 ,7 ,7 ,20 ,2 ,9 ,2 ,18 ,1 ,20 ,5 ,0 ,"Armaduras medias", "Nenhuma", "Kit de alquimia(prof)",
+     "Comum, draconico demoniaco", "Nula", "Não fucionaria assim.", " Bruxas", 1, 4, 0, 7, 7, 3, 4, 5, 4, 6, 7, 8, 5,5,6,7);
