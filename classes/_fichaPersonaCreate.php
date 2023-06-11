dd13aa44d8ef5e0f5afd0468bf85fcd91476bb40
julio ciscato <131544053+fulkie@users.noreply.github.com>
2023-06-11 20:57:39 -0300

    Update _fichaPersonaCreate.php

--- classes/_fichaPersonaCreate.php
+++ classes/_fichaPersonaCreate.php
@@ -52,6 +52,7 @@ $per_investigacao = $_POST["periciaInvestigacao"];
 $per_lidar_animais = $_POST["periciaLidarAnimais"];
 $per_medicina = $_POST["periciaMedicina"];
 $per_natureza = $_POST["periciaNatureza"];
+$per_percepcao = $_POST["periciaPercepcao"];
 $per_persuasao = $_POST["periciaPersuasao"];
 $per_prestidigitacao = $_POST["periciaPrestidigitacao"];
 $per_religiao = $_POST["periciaReligiao"];
@@ -66,7 +67,12 @@ $per_lidar_animais, $per_medicina, $per_natureza, $per_persuasao, $per_prestidig
 $per_religiao, $per_sobrevivencia; */
 
 
-$percp_passiva = $_POST["percepcaoPassiva"];
+$percp_passiva = $_POST["periciaPercepcao"]+10;
+$inves_passiva = $_POST["periciaInvestigacao"]+10;
+$intui_passiva = $_POST["periciaIntuicao"]+10;
+
+
+
 $bonus_profici = $_POST["bonusProciencia"];
 $deslocamento = $_POST["deslocamento"];
 $iniciativa = $_POST["iniciativa"];
@@ -86,17 +92,17 @@ $linguagens = $_POST["linguagens"];
 
 $sql = "INSERT INTO `tb_ficha_personagem`(`cd_usuario`, `nome_jogador`, `nome_personagem`, `raca_personagem`,`nivel_personagem`,`classe_personagem`,
 `antecedente_personagem`, `tendencia_personagem`,`atr_forca`,`atr_destreza`, `atr_constituicao`, `atr_inteligencia`, `atr_sabedoria`, `atr_carisma`,
-`tdr_forca`,`tdr_destreza`, `tdr_constituicao`, `tdr_inteligencia`, `tdr_sabedoria`, `tdr_carisma`,`percp_passiva`, `bonus_profici`,
+`tdr_forca`,`tdr_destreza`, `tdr_constituicao`, `tdr_inteligencia`, `tdr_sabedoria`, `tdr_carisma`,`percp_passiva`, `inves_passiva`, `intui_passiva`, `bonus_profici`,
 `deslocamento`,`iniciativa`,`classe_armadura`,`inspiracao`,`vida_maxima`, `vida_atual`, `per_acrobacia`, `per_arcanismo`, `per_atletismo`, 
-`per_atuacao`, `per_blefar`, `per_furtividade`,`per_historia`, `per_intimidacao`, `per_investigacao`, `per_lidar_animais`, `per_medicina`, 
-`per_natureza`,`per_persuasao`, `per_prestidigitacao`,`per_religiao`,`per_sobrevivencia`,`armor`, `armas`, `ferramentas`, `linguagens`)
+`per_atuacao`, `per_blefar`, `per_furtividade`,`per_historia`, `per_intimidacao`, `per_intuicao`, `per_investigacao`, `per_lidar_animais`, `per_medicina`, 
+`per_natureza`, `per_percepcao`, `per_persuasao`, `per_prestidigitacao`,`per_religiao`,`per_sobrevivencia`,`armor`, `armas`, `ferramentas`, `linguagens`)
  VALUES('$codJogador', '$nome_jogador' , '$nome_personagem', '$raca_personagem', '$nivel_personagem','$classe_personagem',
     '$antecedente_personagem', '$tendencia_personagem','$atr_forca','$atr_destreza', '$atr_constituicao', 
     '$atr_inteligencia', '$atr_sabedoria', '$atr_carisma','$tdr_forca', '$tdr_destreza',
-    '$tdr_constituicao','$tdr_inteligencia', '$tdr_sabedoria', '$tdr_carisma', '$percp_passiva',
+    '$tdr_constituicao','$tdr_inteligencia', '$tdr_sabedoria', '$tdr_carisma', '$percp_passiva', '$inves_passiva', '$intui_passiva',
     '$bonus_profici', '$deslocamento', '$iniciativa', '$classe_armadura','$inspiracao', '$vida_maxima','$vida_atual',
-    '$per_acrobacia', '$per_arcanismo', '$per_atletismo', '$per_atuacao', '$per_blefar', '$per_furtividade','$per_historia', '$per_intimidacao', 
-    '$per_investigacao','$per_lidar_animais', '$per_medicina', '$per_natureza', '$per_persuasao', 
+    '$per_acrobacia', '$per_arcanismo', '$per_atletismo', '$per_atuacao', '$per_blefar', '$per_intuicao', '$per_furtividade','$per_historia', '$per_intimidacao', 
+    '$per_investigacao','$per_lidar_animais', '$per_medicina', '$per_natureza', '$per_percepcao', '$per_persuasao', 
     '$per_prestidigitacao', '$per_religiao', '$per_sobrevivencia', '$armor', '$armas', '$ferramentas', '$linguagens')";
 
     $inserir = mysqli_query($conexao, $sql);
