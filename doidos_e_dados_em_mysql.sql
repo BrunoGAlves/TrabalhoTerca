199ab1da8775e0b63f6252c6adac0d0fa9ac6ad0
julio ciscato <131544053+fulkie@users.noreply.github.com>
2023-06-11 20:58:10 -0300

    Update doidos_e_dados_em_mysql.sql

--- doidos_e_dados_em_mysql.sql
+++ doidos_e_dados_em_mysql.sql
@@ -3,15 +3,6 @@ CREATE DATABASE DOIDOS_E_DADOS
 
 USE DOIDOS_E_DADOS
 
-CREATE TABLE tb_usuarios (
-cd_usuario INT NOT NULL AUTO_INCREMENT,
-usuario_email VARCHAR(50),
-usuario_senha VARCHAR(40),
-usuario_nome VARCHAR(50),
-usuario_apelido VARCHAR(30),
-PRIMARY KEY (cd_usuario))
-
-
 CREATE TABLE tb_ficha_personagem (
 cd_personagem INT NOT NULL AUTO_INCREMENT,
 cd_usuario INT NOT NULL,
@@ -38,6 +29,9 @@ tdr_sabedoria INT NOT NULL,
 tdr_carisma INT NOT NULL,
 
 percp_passiva INT NOT NULL,
+inves_passiva INT NOT NULL,
+intui_passiva INT NOT NULL,
+
 bonus_profici INT NOT NULL,
 deslocamento INT NOT NULL,
 iniciativa INT NOT NULL,
@@ -84,24 +78,3 @@ CONSTRAINT fk_usuarios_personagem FOREIGN KEY (cd_usuario) REFERENCES tb_usuario
 INSERT INTO `tb_usuarios`(`usuario_email`, `usuario_senha`, `usuario_nome`, `usuario_apelido`) VALUES
 ('julio.ciscato@gmail.com', 'teste', 'Julio Ciscato', 'xulio'),
 ('joao.nunes@gmail.com', 'teste', 'João Nunes', 'xoão')
-
-
-INSERT INTO `tb_ficha_personagem`(`cd_usuario`, `nome_jogador`, `nome_personagem`, `raca_personagem`,`nivel_personagem`,`classe_personagem`,
-`antecedente_personagem`, `tendencia_personagem`, `atr_forca`, `atr_destreza`, `atr_constituicao`, `atr_inteligencia`, `atr_sabedoria`, `atr_carisma`, `tdr_forca`, `tdr_destreza`, `tdr_constituicao`, `tdr_inteligencia`, `tdr_sabedoria`, `tdr_carisma`, `percp_passiva`, `bonus_profici`, `deslocamento`, `iniciativa`, `classe_armadura`, 
-`inspiracao`, `vida_maxima`, `vida_atual`,`vida_temporaria`,`per_acrobacia`, `per_arcanismo`, 
-`per_atletismo`, `per_atuacao`, `per_blefar`, `per_furtividade`, `per_historia`, `per_intimidacao`, 
-`per_investigacao`, `per_lidar_animais`, `per_medicina`, `per_natureza`, `per_persuasao`, `per_prestidigitacao`, 
-`per_religiao`, `per_sobrevivencia`)
-VALUES
-(1, 'Julio', 'Passarinho', 'Elfo', 11 ,'Cavaleiro', 'Forasteiro', 'Neutro',
-10, 9, 8, 13, 14, 15,
-0, -1, -1, 1, 2, 2,
-15, 2, 9, -1, 9, 0,
-28, 17, 3,
-2, 3, 4, 5, 3, 4, 5, 6, 5, 4, 3, 2, 6, 5, 4, 3),
-(2, 'João', 'Elizabeth', 'Elfo',11, 'Mago', 'Criminal', 'Caótico',
-14, 9, 8, 13, 10, 11,
-3, -1, -1, 1, 0, 0,
-15, 2, 9, -1, 9, 0,
-28, 17, 3,
-2, 3, 4, 5, 3, 4, 5, 6, 5, 4, 3, 2, 6, 5, 4, 3)
